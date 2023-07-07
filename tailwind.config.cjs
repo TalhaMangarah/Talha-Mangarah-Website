19f5c5a2f0739ec5e7e34986e099ee2d7e997ade
Talha Mangarah <talha@talhamangarah.com>
2023-07-07 02:55:45 +0100

    Updated website/blog & added old blog post

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,12 @@ module.exports = {
       secondary: "#301E67",
       tertiary: "#170d36",
       white: "#ffffff",
+      code_bg: "#24292e"
       // primary: '#271b48'
     },
-    extend: { colors: colors },
+    extend: { 
+      colors: colors
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
